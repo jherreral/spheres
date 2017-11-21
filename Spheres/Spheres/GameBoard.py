975d931c2b7d3af163c335018e6fb35c33092411
jherreral <j.herrera.lv@gmail.com>
2017-11-21 18:30:08 -0300

    Clase de Zonas

--- Spheres/Spheres/GameBoard.py
+++ Spheres/Spheres/GameBoard.py
@@ -1,7 +1,18 @@
+class Zone:
+    def __init__(self, **kwargs):
+        nodeID = None
+        noneName = None
+        sphere = None
+        production = None
+        petrol = None
+        interest = None
+        capital = None
+        return super().__init__(**kwargs)
+
 class GameBoard:
     def __init__(self, **kwargs):
         round = None
-        board_data = None
+        zones_data = []
         edges_pairs = None
         turn_deck = None
         return super().__init__(**kwargs)
